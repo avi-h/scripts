ebde6f35ae941246617088dbbe2feae1aec6ed00
avi-h <you@example.com>
2021-06-28 14:37:59 +0300

    ***

--- python/get-your-age.py
+++ python/get-your-age.py
@@ -8,11 +8,12 @@ while True:
             print('ERROR:year of birth is higher from current year')
         if useryear == current.year:
             print('Welcome to',current.year,'Baby!')
+
+        if (useryear < current.year) and (useryear != 0):
+            age = current.year - useryear
+            print('you are', age, 'year old')
         if useryear == 0:
             print("zero?!! no way, try again")
-        if useryear < current.year:
-            age = current.year - useryear
-            print('you are',age,'years old')
 
     else:
         print('please enter proper data')
