be643cbf54bbfa1c4ad25823adcad850f76b9794
avi-h <you@example.com>
2021-07-08 04:29:33 +0300

    ***

--- DevopsExperts/WorldOfGames/CurrencyRoulleteGame.py
+++ DevopsExperts/WorldOfGames/CurrencyRoulleteGame.py
@@ -1,41 +1,44 @@
 import random
 
 difficulty = 3
-USD_value = 0
+usd_value = 0
 
 
 def generate_USD_value():
-    global USD_value
-    USD_value = random.randint(1, 101)
+    global usd_value
+    usd_value = random.randint(1, 101)
 
 
 def get_money_interval(currency_rate):
-    return (USD_value*currency_rate - 5 + difficulty,USD_value*currency_rate + 5 - difficulty )
+
+
+
+    return (usd_value*currency_rate - 5 + difficulty,usd_value*currency_rate + 5 - difficulty )
 
 
 def get_guess_from_user():
-    no_exception = False  # Flag to keep info whether exception was thrown or not
-    while no_exception == False:
-        print("You chose Currency Roulette Game. Please type your guess of ILS value of " + str(USD_value) + " $ : ")
+    global user__guess
+    no_exception = False  # flag to keep exception info
+    while not no_exception:
+        print("type your guess of ILS value of " + str(usd_value) + " $ : ")
         try:
             user__guess = float(input(">>>"))
             no_exception = True  # The exception was not thrown, so the loop will be finished.
         except ValueError as e:
-            print("\t\t!!!!!!!!!!!!!!!   " + str(e) + "    !!!!!!!!!!!!!!!!!\n")
+            print(str(e))
     return user__guess
 
 def play():
     generate_USD_value()
     guess_from_user = get_guess_from_user()
-    lower_interval  = get_money_interval(3.6)[0]
-    upper_interval = get_money_interval(3.6)[1]
-    print("Interval that system chose is (" + str("{:3.2f}".format(lower_interval)) + "," +
-          str("{:3.2f}".format(upper_interval)) + ") , and your guess is " + str(guess_from_user) + ".")
-    return (guess_from_user <= upper_interval) & (guess_from_user >= lower_interval)
+    lower_interval  = get_money_interval(0)[0]
+    upper_interval = get_money_interval(0)[1]
+    print("system interval: (" + str("{:3.2f}".format(lower_interval)) + "," +
+          str("{:3.2f}".format(upper_interval)) + ")\nyour guess: "
+          + str(guess_from_user))
+    return (guess_from_user <= upper_interval) &\
+           (guess_from_user >= lower_interval)
 
-
-def set_difficulty(difficulty_value):
-    global difficulty
-    difficulty = difficulty_value
 
 play()
+
--- python/CurrencyCalc.py
+++ python/CurrencyCalc.py
@@ -7,11 +7,12 @@ def currency_calc():
             #get currency API and add to a dictionary:
 
             from_rate = input(str('From Currency:'))
+            from_rate_amount = float(input('Mount:'))
             to_rate = input(str('To Currency:'))
             url = 'https://open.er-api.com/v6/latest/'
             results = requests.request('GET', url+from_rate.upper())
 
-            #### create filter the results to dictionaries ###########
+            #### create and filter the results to dictionaries ###########
 
             currency_dict_all = json.loads(results.text)
             currency_from = currency_dict_all['base_code']
@@ -19,8 +20,9 @@ def currency_calc():
             currency_to_desired = currency_to_all[to_rate.upper()]
 
             #### print the results #######
+            currency_to_desired = (from_rate_amount * currency_to_desired)
 
-            print(1,currency_from,'=',currency_to_desired,to_rate.upper())
+            print(from_rate_amount,currency_from,'=',currency_to_desired,to_rate.upper())
         except KeyError:
             print('ERROR:currency was not found')
         except:
