b5996c24e7c9ae8dc54cf8b2153083935f2c6a99
avi-h <you@example.com>
2021-07-14 15:11:58 +0300

    ***

--- Test/WorldOfGames-test/MainGame.py
+++
@@ -1,9 +0,0 @@
-from Live import load_game, welcome
-from Utils import Pause_cmd, run_flask
-import Utils
-
-print(welcome("Guys"))
-load_game()
-# Pause_cmd()
-# run_flask()  # Rise web server to show game scores
-
