7c1bb8b948cde44d83c3373efe87a9d1fddab542
avih.user <avih.user@mail.com>
2021-07-27 00:29:22 +0300

    ***

--- Vagrant/HyperV/CentOS/build.ps1
+++ Vagrant/HyperV/CentOS/build.ps1
@@ -1,23 +1,67 @@
 $Feature = 'Microsoft-Hyper-V-All'
-$OS = 'Centos'
+$OS = 'centos'
 $Vagrantfile = 'https://raw.githubusercontent.com/avi-h/scripts/master/Vagrant/HyperV/CentOS/Vagrantfile'
 $VagrantfileName = 'Vagrantfile'
 $VMpath = "c:\HyperV\Vagrant\$OS"
 $testpath = Test-Path $VMpath
 $FQuery = Get-WindowsOptionalFeature -FeatureName $Feature -Online
+$boxname = 'centos7.box'
+$boxPath = "$VMpath\$boxname"
+$boxImage_Verify = Test-Path $boxPath
+$Provider = 'hyperv'
+
 
 if ($FQuery.State -like 'Enabled') {
 
 if ($testpath -like 'False') {New-Item -Path $VMpath -ItemType Directory -Verbose}
 
+
+Set-Location $VMpath
+
+$VerifyBox = vagrant box list
+
+if ($VerifyBox) {
+
+
+if ($VerifyBox.Remove(7) -like $boxname.Remove(7)) {
+
+Write-Host $boxname.remove(7) "machine found..." -ForegroundColor Yellow
+Set-Location $VMpath
+
+Write-Host "Running vagrant up command..." -ForegroundColor Yellow
+Remove-Item "$VMpath\$VagrantfileName" -Force -ErrorAction SilentlyContinue
+& vagrant init $boxname.Remove(7)
+Write-Host "Downloading Vagrant file to: " $VMpath -ForegroundColor Yellow
+Invoke-WebRequest -Uri $Vagrantfile -OutFile "$VagrantfileName" -Verbose
+& vagrant up --color --timestamp --provider $Provider
+
+} 
+
+elseif ( ($boxImage_Verify -like 'True')  -and
+($VerifyBox.Remove(7) -notlike $boxname.Remove(7) )) {
+
+Write-Host $boxname.Remove(7) "wasnt found, Building..." -ForegroundColor Yellow
+Set-Location $VMpath
+Remove-Item "$VMpath\$VagrantfileName" -Force -ErrorAction SilentlyContinue
+
+& vagrant box add $boxname.Remove(7)  $boxname
+& vagrant init $boxname.Remove(7) 
 Invoke-WebRequest -Uri $Vagrantfile -OutFile "$VMpath\$VagrantfileName" -Verbose
+& vagrant up --color --timestamp --provider $Provider
 
-Set-Location $VMpath -Verbose
-& vagrant up --color --timestamp
+} 
 
+elseif ($boxImage_Verify -like 'False') {
+
+Write-Host "image file" $boxPath "wasnt found." -ForegroundColor Yellow 
+Write-Host $boxname.Remove(7) "Box wasnt created." -ForegroundColor Yellow
+break }
+
+}
 
 } else {
 
 Write-Host "Hyper-V is not installed"
+
 
 }
---
+++ powershell/CreateArray.ps1
@@ -0,0 +1,19 @@
+﻿$Array = @()
+$Computers = "Computer1","Computer2","Computer3"
+
+Foreach ($Computer in $Computers)
+{
+
+$Services = Get-WmiObject Win32_Service -ComputerName $Computer | Select name,state,startmode
+    Foreach ($Service in $Services)
+    {
+    $Row = "" | Select Computer,Service,State,StartupMode
+    $Row.Computer = $Computer
+    $Row.Service = $Service.Name
+    $Row.State = $Service.State
+    $Row.StartupMode = $Service.StartMode
+    $Array += $Row
+    }
+}
+
+$Array
