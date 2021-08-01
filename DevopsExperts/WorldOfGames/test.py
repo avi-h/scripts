60c3e2050f4bbc0b4ac7701fd627256cdaa0be55
avih.user <avih.user@mail.com>
2021-08-01 17:25:59 +0300

    ***

--- DevopsExperts/WorldOfGames/test.py
+++ DevopsExperts/WorldOfGames/test.py
@@ -1,10 +1,14 @@
-def get_money_interval():
-    usd_value = random.randint(1, 101)
-    from_rate = 'usd'
-    url = 'https://open.er-api.com/v6/latest/'
-    results = requests.request('GET', url + from_rate.upper())
-    currency_dict_all = json.loads(results.text)
-    dict_rates = currency_dict_all.get('rates')
-    rate_ils = dict_rates.get('ILS')
+import os
 
-    return ((usd_value*rate_ils - 5 + difficulty),(usd_value*rate_ils + 5 ))
+vm_name = 'ubuntuSRV'
+path = 'C:/VirtualBox/Vagrant/' + vm_name
+folder_verify = os.path.exists(path)
+boxfile = 'vb_ubuntuSRV.box'
+
+if folder_verify == False:
+    os.makedirs(vm_name)
+    print(path)
+    print('Directory', path, 'created.')
+else:
+    print(path,'already exist.')
+
