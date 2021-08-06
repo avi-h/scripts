0ca27f14aad3264133458c6f1cf39f2c86a7c325
avih.user <avih.user@mail.com>
2021-08-06 17:33:06 +0300

    Update base-provision.sh

--- Vagrant/base-provision.sh
+++ Vagrant/base-provision.sh
@@ -9,11 +9,14 @@ yum install docker -y
 yum install gnupg2 -y
 yum install wget -y
 yum install bind-utils mtr -y
+yum install traceroute nslookup -y
 systemctl enable docker
 systemctl start docker
 systemctl stop firewalld
 systemctl disable firewalld
 hostnamectl set-hostname centos7
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod 777 /usr/local/bin/docker-compose
 curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 sudo yum install -y powershell
 sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
