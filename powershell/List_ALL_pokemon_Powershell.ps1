da7b0e5ba1f0239c6005fff53cbfe4442e333761
avih.user <avih.user@mail.com>
2021-08-10 19:19:46 +0300

    ***
    ***

---
+++ powershell/List_ALL_pokemon_Powershell.ps1
@@ -0,0 +1,65 @@
+﻿#Script Description
+
+
+do {
+#URLs
+$url01 = 'https://pokeapi.co/api/v2/pokemon-species'
+$url02 = 'https://pokeapi.co/api/v2/pokemon/'
+#API Request
+$buffer_size = (Invoke-RestMethod -Method Get -Uri $url01).count
+$results01 = Invoke-RestMethod -Method Get -Uri ($url01 + "?limit=$buffer_size" )
+
+
+#get all objects and add them to a Table
+$poke_list = $results01.results | 
+select @{l='Number'
+e={(($_.url)-replace($url01))-replace('/') }} , 
+
+@{l='Name'
+e={$_.name}}
+
+#----end of table and data collection---------------------
+
+
+$count = $poke_list.Count
+
+#output results
+$poke_list | Out-String
+
+try {
+
+
+[int]$select = Read-Host "Select Pokemon Number(1-$count)"
+
+#data input validation & error handaling
+if ($select -and $select -le $count) {
+$selection = $poke_list | ? {$_.number -like $select
+}
+
+Write-Host $selection.Name "is Selected" -ForegroundColor Green -BackgroundColor Black
+
+if ($selection.Number -eq 1) {
+Write-Host $selection.Name "is the first name in the Evolution-Chain!" -ForegroundColor Yellow -BackgroundColor Black
+
+}
+
+$results02 = Invoke-RestMethod -Method Get -Uri ($url02 + ($selection.Name).ToLower()+$buffering)
+$results02 | fl Name,ID,Weight,Height
+
+ } 
+
+ else {Write-Warning "your choice is not in range"}
+    
+ } catch {
+                
+        
+Write-Host "No Data found. Try another" -ForegroundColor Red -BackgroundColor black
+
+
+}
+
+$run = Read-Host "Run Again ?(y/n)"
+
+    } while ($run -like 'y')
+
+#script will run in a loop based on user's choice
---
+++ powershell/Solution02_pokemon_Powershell.ps1
@@ -0,0 +1,48 @@
+﻿
+#URLs
+$url = 'https://pokeapi.co/api/v2/'
+$endpoint01 = 'pokemon-species'
+$endpoint02 = 'evolution-chain'
+
+
+
+#API Request
+$buffer_size = (Invoke-RestMethod -Method Get -Uri ($url + $endpoint01)).count
+$results_names = Invoke-RestMethod -Method Get -Uri ($url + $endpoint01 + "?limit=$buffer_size" )
+
+
+#get all objects and add them to a Table
+$poke_list = $results_names.results | 
+select @{l='Number'
+e={(($_.url)-replace($url01))-replace('/') }} , 
+
+@{l='Name'
+e={$_.name}}
+
+#----end of table and data collection---------------------
+
+$pokemon_name = (Read-Host "Enter Pokemon Name")
+
+$poke_verify = $poke_list | ? {$_.name -like $pokemon_name}
+
+if ($pokemon_name -like $poke_verify.Name) {
+
+write-host $pokemon_name "was found and listed" -ForegroundColor Green -BackgroundColor Black
+
+if ($poke_verify.Number -like 1) {
+Write-Host $pokemon_name "is first in evolution chain" -ForegroundColor Yellow -BackgroundColor Black
+}
+
+$pre_chain = ([int]$poke_verify.Number - 1)
+
+$pre_chain_poke = $poke_list | ? {$_.Number -like $pre_chain}
+
+Write-Host $pokemon_name "evolves from" $pre_chain_poke.Name -ForegroundColor Magenta -BackgroundColor Black
+
+} else {
+
+Write-Warning "$pokemon_name doesnt exist, try another Pokemon"
+
+}
+
+Write-Host "NOTE:To view all listed pokemons plus their properties, run List_All_Powershell.ps1" -ForegroundColor Yellow -BackgroundColor Black
