adb8c327e675c521ca272d66165af683ae1bdc39
avih.user <avih.user@mail.com>
2021-08-16 18:54:34 +0300

    Update docker-bash.sh

--- DevopsExperts/Docker/zabbix/docker-bash.sh
+++ DevopsExperts/Docker/zabbix/docker-bash.sh
@@ -1,4 +1,4 @@
-docker network create zabbix-net
+#docker network create zabbix-net
 docker run --name mysql --net zabbix-net -p 3306:3306 \
 -e MYSQL_ROOT_PASSWORD=1q2w3e4r \
 -e MYSQL_USER=zabbix \
@@ -6,3 +6,16 @@ docker run --name mysql --net zabbix-net -p 3306:3306 \
 -e MYSQL_DATABASE=zabbix \
 -d mysql
 #------------------------------------
+docker run --name zabbix-srv --net zabbix-net -p 10051:10051 \
+-e DB_SERVER_HOST=mysql \
+-e MYSQL_USER=zabbix \
+-e MYSQL_PASSWORD=zabbix \
+-d zabbix/zabbix-server-mysql
+#---------------------------------------
+docker run --name zabbix-web --net zabbix-net -p 88:8080 \
+-e DB_SERVER_HOST=zabbix-srv \
+-e MYSQL_USER=zabbix \
+-e MYSQL_PASSWORD=zabbix \
+-e ZBX_SERVER_HOST=zabbix-srv \
+-e PHP_TZ=Asia/Jerusalem \
+-d zabbix/zabbix-web-apache-mysql
