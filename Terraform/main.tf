742f19a4fcac9884cb59518e324a876832466bac
avih.user <avih.user@mail.com>
2021-11-01 20:39:13 +0200

    *
    *

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,9 +43,10 @@ resource "aws_security_group" "sec-grp-devops" {
 #create key pair to push the instances
 resource "aws_key_pair" "alegria-lap" {
   key_name   = "alegria-lap"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZqAvvd4hYJqcW/Z6ScQwZFvHAeSKcKVk1RPKzg/nNk9vOt1T0YlzpkzRVLW9NG6vAs6vY2ZwDu2rBLVULSMnhBNvkkYdm7kbqZgJ/nlp6OV+LEvMpq2LVgHI4yWCxt02limGylkPsavGP9FrVW0UV1mWG8ObPQ8mfF1YhYfbh4h49q6ffsAuFAKQLoq///Ih8ikuoEJeZdFkN9NU9ofKlAxBw5D1W1yORYae8kAqXsg5AJaa/SFV/OsUsiEI6SZY2gyRePMRupoLKJS9t1AtqFao/MXa97QA0R5EJq4BUri3/+nlzMs5I/nSizUzIGnIdR1u+B36aiRtwaUfVT0BelPAVmPnaHhncdjVriJozIp4dh5KSdfqaYjZvEL28SLj9P9dEjmVQ8yy9pu69E1UCz9eApkmKB472DJrsDR/pWY5dzUwxxtsahL70J/feuRgnR892yPOwkrRGRi1U29jtTJMm/FIK8V/6TfxdzoxImgKpXpgWfLEFF7I8ux1RSEM= alegria@Alegria-Lap01"
+  public_key = file("C:/Data/AWS/keys/alegria_id_rsa.pub")
 }
 
+/* ----- create instances -----------------------
 resource "aws_instance" "terraform-web01" {
   ami = "ami-0747bdcabd34c712a"
   instance_type = "t2.micro"
@@ -57,7 +58,6 @@ resource "aws_instance" "terraform-web01" {
 
 }
 
-#create instances
 resource "aws_instance" "terraform-web02" {
   ami = "ami-0747bdcabd34c712a"
   instance_type = "t2.micro"
@@ -67,9 +67,24 @@ resource "aws_instance" "terraform-web02" {
   security_groups = ["sec-grp-devops"]
   key_name = "alegria-lap"
 }
+----------------------------------------------------- */
+#create instances based on count Module.
+resource "aws_instance" "web0" {
+  count = 2
+  ami = "ami-0747bdcabd34c712a"
+  instance_type = "t2.micro"
+  security_groups = ["sec-grp-devops"]
+  key_name = "alegria-lap"
+  tags = {
+    Name = "Web0${count.index}"
+  }
 
-#create load balancer
-resource "aws_elb" "lb-LaKukaracha" {
+}
+#--------------------------------------
+
+
+/*-------create load balancer--------------------
+  resource "aws_elb" "lb-LaKukaracha" {
   name               = "lb-LaKukaracha"
   internal           = false
   subnets            = ["subnet-9a1fedd6", "subnet-7b123d24",
@@ -81,25 +96,25 @@ resource "aws_elb" "lb-LaKukaracha" {
     lb_port           = 80
     lb_protocol       = "http"
   }
-  listener {
-    instance_port     = 22
-    instance_protocol = "tcp"
-    lb_port           = 22
-    lb_protocol       = "tcp"
-  }
 
   instances       = [aws_instance.terraform-web01.id,aws_instance.terraform-web02.id]
   security_groups = [aws_security_group.sec-grp-devops.id]
 
 }
 
-#print elb and servers public addresses
-output "aws_elb" {
+-------print results-----------
+ output "aws_elb" {
   value = "http://${aws_elb.lb-LaKukaracha.dns_name}"
 }
+
+
 output "public_dns" {
-  value = "web01 = ssh ubuntu@${aws_instance.terraform-web01.public_dns}"
+  value = "1. ssh ubuntu@${aws_instance.terraform-web01.public_dns}"
 }
 output "aws_instance" {
   value = "web02 = ssh ubuntu@${aws_instance.terraform-web02.public_dns}"
+} */
+
+output "aws_instance" {
+  value = "web02 = ssh ubuntu@${aws_instance.web0[count.index]}"
 }
---
+++ powershell/char_list-example.ps1
@@ -0,0 +1,8 @@
+﻿$alph=@()
+65..90|
+
+foreach {
+$alph+=[char]$_ + '.'
+}
+
+$alph
