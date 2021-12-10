c40893640251554c76bcc035ab37f760b3fcd917
avi.h <glpat-bkyWEDzYC_r_CSbJxUVS>
2021-12-10 10:05:02 +0200

    Create reset-pass-localuser.ps1

---
+++ powershell/reset-pass-localuser.ps1
@@ -0,0 +1,7 @@
+﻿$computers = 'localhost'
+$user = Read-Host 'username'
+$pass = Read-Host 'password'
+foreach($computer in $computers) {   
+    $localUser = [ADSI]  "WinNT://$computerName/$user"  
+    $localUser.SetPassword($Pass)  
+} 
