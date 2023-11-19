fd0dcd5b2a20a0e600c3a9b65d02b5da093ff73a
avih.user <avih.user@mail.com>
2023-11-19 20:32:05 +0200

    ***

--- Vagrant/base-provision.sh
+++ Vagrant/base-provision.sh
@@ -14,10 +14,12 @@ systemctl enable docker
 systemctl start docker
 systemctl stop firewalld
 systemctl disable firewalld
-hostnamectl set-hostname centos7
+#hostnamectl set-hostname centos7
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod 777 /usr/local/bin/docker-compose
 curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+yum update -y
 sudo yum install -y powershell
+pwsh --version
 sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 shutdown -r now
