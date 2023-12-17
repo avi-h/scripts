0fa71cbaf63217b98e22f314b959c69a4356f6cb
avih.user <avih.user@mail.com>
2023-12-17 19:02:15 +0200

    *

---
+++ powershell/Bulk_Rename.ps1
@@ -0,0 +1,16 @@
+﻿$Directory = 'C:\MyData\Pictures'
+$NewName = 'image_0'
+$extention = '.jpg'
+
+
+$items = Get-ChildItem -Path $Directory
+
+$RenameAction = $items | 
+
+% {$count = 0} {$count++ ; 
+Rename-Item -Path $_.FullName -NewName ($NewName + $count + $extention)}
+
+
+(Get-ChildItem $Directory).FullName
+
+
---
+++ powershell/char_list-example2.ps1
@@ -0,0 +1,14 @@
+﻿#list items by numbers
+
+(Gps).Name | 
+% {$start=0 } { $start++ ; "$start.$_"}
+
+#------------------------------------------
+
+#for loop
+for ($test = 1 ; $test -lt ((Get-ChildItem C:\data).count)+1  ; $test++)
+{
+[string]$test + '.' 
+}
+
+#----------------
