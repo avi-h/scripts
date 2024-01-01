8703bbc82b6d0095dcdae3da8b9ad9d2591855a0
alegria <alegria@alegria-lap01.local>
2024-01-01 23:22:21 +0200

    *

--- powershell/CurrencyCalc.ps1
+++ powershell/CurrencyCalc.ps1
@@ -1,27 +1,19 @@
-﻿
-
-
-$FromCurrancy = Read-Host "From"
-[int]$FromAmount = Read-Host 'Amount'
-$ToRate = Read-Host "To"
+﻿$FromCurrency = Read-Host "From Currency"
+[decimal]$FromAmount = Read-Host 'Amount'
+$ToRate = Read-Host "To Currency"
 
 if ($FromAmount) {
 
-$uri = 'https://open.er-api.com/v6/latest/' + ($FromCurrancy).ToUpper()
+$uri = 'https://open.er-api.com/v6/latest/' + ($FromCurrency).ToString().ToUpper()
 
 $response = Invoke-RestMethod $uri -Method 'GET'
 
 
-Write-Host $FromAmount $response.base_code =  ($response.rates.$ToRate * $FromAmount) $ToRate.ToUpper() -ForegroundColor Green -BackgroundColor Black
+Write-Host $FromAmount $response.base_code =  ($response.rates.$ToRate * $FromAmount) $ToRate.ToString().ToUpper() -ForegroundColor Green -BackgroundColor Black
 
 } else {
 
 "No valid input"
 
-
 }
-
-
-
-
 
