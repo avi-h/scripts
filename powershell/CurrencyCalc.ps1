70ae9a700d9e819ba2c05305d6331a205bedf556
alegria <alegria@alegria-lap01.local>
2024-01-03 00:08:43 +0200

    *

--- powershell/CurrencyCalc.ps1
+++ powershell/CurrencyCalc.ps1
@@ -1,19 +1,19 @@
 ﻿$FromCurrency = Read-Host "From Currency"
-[decimal]$FromAmount = Read-Host 'Amount'
-$ToRate = Read-Host "To Currency"
+try {
+[decimal]$Sum = Read-Host 'Sum(Digits Only)'
+} catch {'Wrong input format'}
+$ToCurrency = Read-Host "To Currency"
 
-if ($FromAmount) {
+$uri = 'https://open.er-api.com/v6/latest/'
+$response = Invoke-RestMethod ($uri + ($FromCurrency).ToString().ToUpper()) -Method 'GET'
+$response_ToCurrency = Invoke-RestMethod ($uri + ($ToCurrency).ToString().ToUpper()) -Method 'GET'
 
-$uri = 'https://open.er-api.com/v6/latest/' + ($FromCurrency).ToString().ToUpper()
+if ($response.result -match 'Success' -and $response_ToCurrency.result -match 'Success') {
 
-$response = Invoke-RestMethod $uri -Method 'GET'
-
-
-Write-Host $FromAmount $response.base_code =  ($response.rates.$ToRate * $FromAmount) $ToRate.ToString().ToUpper() -ForegroundColor Green -BackgroundColor Black
+Write-Host $Sum $response.base_code =  ($response.rates.$ToCurrency * $Sum) $ToCurrency.ToString().ToUpper() -ForegroundColor Green -BackgroundColor Black
 
 } else {
-
-"No valid input"
+Write-Host 'FromCurrency_Code:' $response.result -ForegroundColor Red -BackgroundColor Black
+Write-Host 'ToCurrency_Code:' $response_ToCurrency.result -ForegroundColor Red -BackgroundColor Black
 
 }
-
