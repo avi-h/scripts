fd89fdd2c191ff2e3c67ee00a45b9fc227f500c0
alegria <alegria@alegria-lap01.local>
2024-01-03 19:57:30 +0200

    *

--- powershell/CurrencyCalc.ps1
+++ powershell/CurrencyCalc.ps1
@@ -1,19 +1,33 @@
-﻿$FromCurrency = Read-Host "From Currency"
+﻿#Currency calc with error handling
+do {
+
 try {
-[decimal]$Sum = Read-Host 'Sum(Digits Only)'
-} catch {'Wrong input format'}
-$ToCurrency = Read-Host "To Currency"
+
+$UserInputValid = $true
+$FromCurrency = Read-Host "1.From which Currency ?"
+[decimal]$Sum = Read-Host "2.How Much ?(Numbers)"
+$ToCurrency =   Read-Host "3.To which Currency ?"
 
 $uri = 'https://open.er-api.com/v6/latest/'
-$response = Invoke-RestMethod ($uri + ($FromCurrency).ToString().ToUpper()) -Method 'GET'
-$response_ToCurrency = Invoke-RestMethod ($uri + ($ToCurrency).ToString().ToUpper()) -Method 'GET'
 
-if ($response.result -match 'Success' -and $response_ToCurrency.result -match 'Success') {
+$response = Invoke-RestMethod ($uri + ($FromCurrency).ToString().ToUpper()) -Method 'GET'
+$response_ToCurrency = Invoke-RestMethod ($uri + ($ToCurrency).ToString().ToUpper()) -Method 'GET' 
+
+} catch {
+Write-Host "User input Error" -ForegroundColor Red -BackgroundColor Black
+$UserInputValid = $false
+}
+
+if ($UserInputValid -match $true) {
+
+if (($response.result -match 'success') -and 
+($response_ToCurrency.result -match 'success')) {
 
 Write-Host $Sum $response.base_code =  ($response.rates.$ToCurrency * $Sum) $ToCurrency.ToString().ToUpper() -ForegroundColor Green -BackgroundColor Black
 
-} else {
-Write-Host 'FromCurrency_Code:' $response.result -ForegroundColor Red -BackgroundColor Black
-Write-Host 'ToCurrency_Code:' $response_ToCurrency.result -ForegroundColor Red -BackgroundColor Black
+} else {Write-Host "Error in url request" -ForegroundColor Red -BackgroundColor Black}
+  }
 
-}
+$re_run = Read-Host 'run again?(y/n)'
+
+} while ($re_run -match 'y')
