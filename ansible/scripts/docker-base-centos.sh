c9daeee87ce821e44cd70db5dd7813f69b608769
alegria <alegria@alegria-desk.local>
2024-01-15 00:29:01 +0200

    *

---
+++ ansible/scripts/docker-base-centos.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+#yum repolist
+#yum update -y
+#yum install bash-completion -y
+#yum install bash-completion-extras -y
+#yum install net-tools -y
+#yum install vim -y
+#yum install tcpdump -y
+#yum install gnupg2 -y
+#yum install wget -y
+#yum install bind-utils mtr telnet -y
+#yum install traceroute nslookup -y
+#yum install openssh-server -y
+#yum update -y
+#yum upgrade -y
---
+++ ansible/scripts/docker-base-ubuntu.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+apt-get update -y
+apt-get install bash-completion -y
+apt-get install bash-completion-extras -y
+apt-get install net-tools -y
+apt-get install vim -y
+apt-get install tcpdump -y
+apt-get install gnupg2 -y
+apt-get install wget -y
+apt-get install bind-utils mtr -y
+apt-get install traceroute nslookup telnet -y
+apt-get update -y
+apt-get upgrade -y
