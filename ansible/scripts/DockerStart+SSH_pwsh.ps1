6fa27c232f8b381173a110e47b868c0e68b3300a
alegria <alegria@alegria-desk.local>
2024-01-18 11:18:28 +0200

    *

---
+++ ansible/scripts/DockerStart+SSH_pwsh.ps1
@@ -0,0 +1,25 @@
+﻿#add container name here
+$containers = @(
+'test02',
+'test03',
+'test04')
+#------------------------
+
+$status = docker ps -a | sls 'exit'
+
+$servicepath = '/usr/sbin/sshd'
+
+
+if ($status) {
+
+foreach ($container in $containers) {
+docker start $container
+$OsType = docker exec $container cat /etc/os-release | sls '^ID='
+#$OsType = ($OsType -replace 'ID=','')-replace '"',''
+
+if ($OsType -match 'centos') {docker exec $container $servicepath}
+if ($OsType -match 'ubuntu') {docker exec $container service ssh start}
+ 
+ }
+     
+    }
