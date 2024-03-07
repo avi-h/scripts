b0becba779a4b92ea71ef5d24e0312b5ffa93c12
alegria <alegria@alegria-desk.local>
2024-03-07 22:56:28 +0200

    *

---
+++ SecurePass_Methods.ps1
@@ -0,0 +1,19 @@
+﻿#store & secured password - example
+$SecureUserInput = Read-Host 'enter password' -AsSecureString
+$EncryptedInput = ConvertFrom-SecureString -SecureString $SecureUserInput
+$SecureString = ConvertTo-SecureString -String $EncryptedInput
+
+#===============================================================================
+#other_way - PS vault
+
+Install-Module -Name Microsoft.PowerShell.SecretManagement -Repository PSGallery -Scope AllUsers
+
+Install-Module -Name Microsoft.PowerShell.SecretStore -Repository PSGallery -Scope AllUsers
+#create a vault
+Register-SecretVault -Name HolaVault -ModuleName microsoft.powershell.SecretStore -DefaultVault
+
+#create a secret
+Set-Secret -Name MyToken -Secret 'Pass@word!'
+
+<# if its your first secret to be saved in the vault,
+PowerShell will prompt you for a password #>
---
+++ powershell/AddUsersFromCSV.ps1
@@ -0,0 +1,43 @@
+﻿#UserList
+$users = Import-Csv 'C:\temp\users.csv'
+
+#generate random password (function)
+function Generate-Password {
+    param (
+        [Parameter(Mandatory)]
+        [int] $length,
+        [int] $amountOfNonAlphanumeric = 1
+    )
+    Add-Type -AssemblyName 'System.Web'
+    return [System.Web.Security.Membership]::GeneratePassword($length, $amountOfNonAlphanumeric)
+}
+ 
+#map the CSV file values to var
+foreach ($user in $users) {
+
+$displayname = $user.Name
+$name = $user.Name
+$fname = $user.GivenName
+$lname = $user.Surname
+$sam = $user.SamAccountName
+$upn = $user.UserPrincipalName
+$department = $user.Department
+$ou = $user.OU
+$pass = Generate-Password -length 8
+$SecPass = ConvertTo-SecureString -String $pass -AsPlainText -Force
+
+#create the users
+New-ADUser -Name $name -DisplayName $displayname -GivenName $fname `
+           -Surname $lname -SamAccountName $sam -UserPrincipalName $upn `
+           -Department $department -Path $ou -Enabled $true -ChangePasswordAtLogon $true `
+           -AccountPassword $SecPass
+
+#verify if user existed in AD
+$verifyUser = (Get-ADUser $user.SamAccountName).SamAccountName
+
+#add and display the results in a table
+$result = @{$verifyUser = $pass}
+$result | select @{l='UserName';e={$_.keys}} ,
+@{l='TempPass';e={$_.values}}
+
+}
